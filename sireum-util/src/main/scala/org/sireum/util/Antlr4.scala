7fc5a73c0613826ab6ba0b0b231c0a9871c1ea5d
Robby <robby@k-state.edu>
2014-02-20 16:04:08 -0600

    Added utility object for parsing using Antlr4.

---
+++ sireum-util/src/main/scala/org/sireum/util/Antlr4.scala
@@ -0,0 +1,109 @@
+/*
+Copyright (c) 2014 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+package org.sireum.util
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+object Antlr4 {
+  import org.antlr.v4.runtime._
+  import org.antlr.v4.runtime.tree._
+
+  trait Visitor[T <: PropertyProvider] extends ParseTreeVisitor[T] {
+    def getChildren[T, PT <: ParseTree](
+      trees : java.util.List[PT]) : ISeq[T] = {
+      var children = ivectorEmpty[T]
+      if (trees != null) {
+        import scala.collection.JavaConversions._
+        for (tree <- trees)
+          children :+= getChild(tree)
+      }
+      children
+    }
+
+    def getChild[T](tree : ParseTree) : T =
+      visit(tree).asInstanceOf[T]
+
+    def getOptChild[T](tree : ParseTree) : Option[T] =
+      if (tree == null) None
+      else Some(visit(tree).asInstanceOf[T])
+  }
+
+  implicit class Location[T <: PropertyProvider](
+      val node : T) extends AnyVal {
+    import org.sireum.util.{ Location => L }
+
+    def at(ctx : ParserRuleContext, locPropKey : String = L.locPropKey)(
+      implicit source : Option[FileResourceUri]) : T = {
+      val start = ctx.start
+      val stop = if (ctx.stop == null) ctx.start else ctx.stop
+      at(start, stop, locPropKey)(source)
+    }
+
+    def at(tn : org.antlr.v4.runtime.tree.TerminalNode)(
+      implicit source : Option[FileResourceUri]) : T =
+      at(tn, L.locPropKey)(source)
+
+    def at(tn : org.antlr.v4.runtime.tree.TerminalNode,
+           locPropKey : String)(
+             implicit source : Option[FileResourceUri]) : T =
+      at(tn.getSymbol, locPropKey)(source)
+
+    def at(tnBegin : org.antlr.v4.runtime.tree.TerminalNode,
+           tnEnd : org.antlr.v4.runtime.tree.TerminalNode)(
+             implicit source : Option[FileResourceUri]) : T =
+      at(tnBegin, tnEnd, L.locPropKey)(source)
+
+    def at(tnBegin : org.antlr.v4.runtime.tree.TerminalNode,
+           tnEnd : org.antlr.v4.runtime.tree.TerminalNode,
+           locPropKey : String)(
+             implicit source : Option[FileResourceUri]) : T =
+      at(tnBegin.getSymbol, tnEnd.getSymbol, locPropKey)(source)
+
+    def at(start : Token, stop : Token)(
+      implicit source : Option[FileResourceUri]) : T =
+      at(start, stop, L.locPropKey)(source)
+
+    def at(start : Token, stop : Token, locPropKey : String)(
+      implicit source : Option[FileResourceUri]) : T = {
+      val lb = stop.getLine
+      val cb = stop.getCharPositionInLine
+      val (le, ce) = end(lb, cb, stop.getText)
+      SourceOffsetLocation.pp2sol(node)(locPropKey).at(
+        fileUri = source,
+        offset = start.getStartIndex,
+        length = stop.getStopIndex - start.getStartIndex,
+        lineBegin = start.getLine,
+        columnBegin = start.getCharPositionInLine,
+        lineEnd = le,
+        columnEnd = ce)
+      node
+    }
+
+    def at(t : Token)(
+      implicit source : Option[FileResourceUri]) : T =
+      at(t, t, L.locPropKey)(source)
+
+    def at(t : Token, locPropKey : String)(
+      implicit source : Option[FileResourceUri]) : T =
+      at(t, t, locPropKey)(source)
+
+    private def end(
+      lineBegin : Int, columnBegin : Int, text : String) : (Int, Int) = {
+      val i = text.lastIndexOf('\n')
+      if (i < 0)
+        (lineBegin, columnBegin + text.length - 1)
+      else {
+        var lines = 0
+        for (c <- text if c == '\n') lines += 1
+        (lineBegin + lines, text.length - i - 3)
+      }
+    }
+  }
+}
